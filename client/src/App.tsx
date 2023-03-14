c26a4abc67b6a4de86c5a31e7d604bf18de2f7a0
Samantha Tran <stran@Samanthas-MacBook-Air.local>
2023-03-14 15:53:47 +1000

    add prettier

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,13 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
+import { useState } from "react"
+import reactLogo from "./assets/react.svg"
+import "./App.css"
 
 function App() {
-  return (
-    <div className="App">
-      <p className="bg-pink-200">Hello</p>
-    </div>
-  );
+    return (
+        <div className="App">
+            <p className="bg-pink-200">Hello</p>
+        </div>
+    )
 }
 
-export default App;
+export default App
